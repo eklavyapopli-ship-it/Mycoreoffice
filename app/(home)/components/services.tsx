f37b6cdf1a62f14cb9afeb4b6c0f28ab342cb893
eklavyapopli-ship-it <eklavyapopli@gmail.com>
2026-03-08 14:37:18 +0530

    minor api bug fix

--- app/(home)/components/services.tsx
+++ app/(home)/components/services.tsx
@@ -64,7 +64,7 @@ export default function ProductsPage({ limit }: any) {
     }
 
     try {
-      const res = await fetch("/api/middle", {
+      const res = await fetch("/api/form", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, phone, message }),
