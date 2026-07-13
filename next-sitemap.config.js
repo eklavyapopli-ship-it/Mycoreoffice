a0d75813cc40f01a370d195b395cc5fbbf49f49d
eklavyapopli-ship-it <eklavyapopli@gmail.com>
2026-07-13 13:52:23 +0530

    sitemap and robots fixes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.mycoreoffice.com', // Replace with your actual site URL
+  siteUrl: 'https://mycoreoffice.com', // Replace with your actual site URL
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
